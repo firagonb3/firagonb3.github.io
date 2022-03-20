eff698414605f635264ef06253acbab0a999b9d8
firagonb3 <93060162+firagonb3@users.noreply.github.com>
2022-03-20 16:57:16 +0100

    Add files via upload

---
+++ categorias/cosplays/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <?php include('../../recursos/php/host.php');?>
+</head>
+
+<body>
+  <header>
+    <!--Menu-->
+    <?php require($uri . "/recursos/index/header.html") ?>
+  </header>
+  <main>
+    <div class="container">
+      <br>
+      <?php include('php/feed_Cosplay.php')?>
+      <!--Noticias-->
+
+      <div id="div_Gral" class="row row-cols-1 row-cols-md-4 g-4">
+        <?php 
+          feed("https://www.levelup.com/rss", 4);
+          feed("https://vandal.elespanol.com/xml.cgi", 2);
+          feed("https://www.egames.news/rss/feed.xml", 5);
+          feed("https://www.eurogamer.es/?format=rss&type=news", 4);
+        ?>
+      </div>
+    </div>
+    <div class="botoncito-container">
+      <div class="botoncito">
+        <button type="button" class="btn btn-outline-primary">
+          <svg xmlns="http://www.w3.org/2000/svg" width="29" height="50" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevrons-up"><polyline points="17 11 12 6 7 11"></polyline><polyline points="17 18 12 13 7 18"></polyline></svg>
+        </button>
+      </div>
+    </div>
+  </main>
+  <footer>
+    <?php require($uri . "/recursos/index/footer.html");?>
+  </footer>
+</body>
+
+</html>
---
+++ categorias/cosplays/php/feed_Cosplay.php
@@ -0,0 +1,88 @@
+<?php
+    function Cosplay($cadena) {
+        if (preg_match('@Cosplay@', $cadena) or preg_match('@cosplay@', $cadena)) {
+            return true;
+        }
+        return false;
+    }
+
+    function extraerSRC($cadena,$Preg) {
+        if ($Preg == 0) {
+            preg_match('@target="_blank"><img src="([^"]+)"@', $cadena, $array);
+        } else {
+            preg_match('@src="([^"]+)"@', $cadena, $array);
+        }
+        $src = array_pop($array);
+        return $src;
+    }
+
+    function feed($feedURL, $IMG){
+        $i = 0; 
+        $url = $feedURL; 
+        $rss = simplexml_load_file($url); 
+        foreach($rss->channel->item as $item) { 
+            $link = $item->link;  //extrae el link
+            $title = $item->title;  //extrae el titulo
+            $date = $item->pubDate;  //extrae la fecha
+
+            $description = strip_tags($item->description);  //extrae la descriCosplayion
+            $description2 = strip_tags($item->description);  //extrae la descriCosplayion
+
+            if (strlen($description) > 100) { //limita la descriCosplayion a 200 caracteres
+                $stringCut = substr($description, 0, 100);                   
+                $description = substr($stringCut, 0, strrpos($stringCut, ' ')).'...';
+            }
+            
+            if($IMG == 0){          //extrae el link de la imagen
+                $url_imagen = $item->guid;  
+            }
+            
+            if($IMG == 1){
+                if(is_array(($item->xpath('media:thumbnail')))){
+                    $mediaArray = $item->xpath('media:thumbnail');                                
+                    $media = end($mediaArray);                                              
+                    $url_imagen = $media->attributes()->url;
+                }
+            }
+
+            if ($IMG == 2) {
+                $url_imagen = $item->description;
+                $url_imagen = extraerSRC($url_imagen,0);
+            }
+
+            if ($IMG == 3) {
+                $url_imagen = $item->enclosure->attributes()->url;
+            }
+
+            if ($IMG == 4) {
+                $url_imagen = $item->description;
+                $url_imagen = extraerSRC($url_imagen,1);
+            }
+
+            if($IMG == 5){
+                if(is_array(($item->xpath('media:content')))){
+                    $mediaArray = $item->xpath('media:content');                                
+                    $media = end($mediaArray);                                              
+                    $url_imagen = $media->attributes()->url;
+                }
+            }
+            if (Cosplay($title) or Cosplay($description2)) {
+                echo "
+                    <div class='col'  id='". $i ."'>
+                        <div class='card h-100'>
+                            <img src='" . $url_imagen . "' class='card-img-top' alt='...'  width='304' height='200'>
+                            <div class='card-body d-flex flex-column'>
+                                <h5 class='card-title'>" . $title . "</h5>
+                                <p class='card-text'>" .  $description . "</p>
+                                <a href='" . $link . "' class='mt-auto btn btn-primary' style='margin-right: 150px;'>Leer mas...</a>
+                            </div>
+                            <div class='card-footer'>
+                                <small class='text-muted'>" . $date . "</small>
+                            </div>
+                        </div>
+                    </div>"
+                ;
+            }
+        }
+    }
+?>
